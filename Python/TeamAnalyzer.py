c11f5ede30d15c228e3732cdbbfc9aeb74709f93
msurve2@uw.edu <msurve2@uw.edu>
2023-05-01 16:06:34 -0700

    finished

--- Python/TeamAnalyzer.py
+++ Python/TeamAnalyzer.py
@@ -12,9 +12,6 @@ if len(sys.argv) < 6:
     sys.exit()
 
 team = []
-for i, arg in enumerate(sys.argv):
-    if i == 0:
-        continue
 
     # Analyze the pokemon whose pokedex_number is in "arg"
 
@@ -22,6 +19,71 @@ for i, arg in enumerate(sys.argv):
     # Remember to look at those "against_NNN" column values; greater than 1
     # means the Pokemon is strong against that type, and less than 1 means
     # the Pokemon is weak against that type
+
+connection = sqlite3.connect('/Users/maansisurve/Desktop/INFO330/INFO330-AccessingDatabases/pokemon.sqlite')
+table = connection.cursor()
+
+for i, arg in enumerate(sys.argv):
+    if i == 0:
+        continue
+    table.execute('SELECT name FROM imported_pokemon_data WHERE pokedex_number = ?', (arg,))
+    name = table.fetchone()
+
+    table.execute('SELECT pokedex_number FROM imported_pokemon_data WHERE name = ?', (arg,))
+    number = table.fetchone()
+
+    table.execute('SELECT type1 FROM imported_pokemon_data WHERE pokedex_number = ?', (arg,))
+    type1 = table.fetchone()
+
+    table.execute('SELECT type1 FROM imported_pokemon_data WHERE name = ?', (arg,))
+    hi = table.fetchone()
+
+    table.execute('SELECT type2 FROM imported_pokemon_data WHERE pokedex_number = ?', (arg,))
+    type2 = table.fetchone()
+
+    table.execute('SELECT type2 FROM imported_pokemon_data WHERE name = ?', (arg,))
+    bye = table.fetchone()
+
+    strong_values = []
+    for type in types:
+        table.execute(f"SELECT against_{type} FROM imported_pokemon_data WHERE against_{type} > 1 AND pokedex_number = ?", (arg,))
+        collection = table.fetchall()
+
+        if collection:
+            strong_values.append(type)
+
+    strong_valuesA = []
+    for type in types:
+        table.execute(f"SELECT against_{type} FROM imported_pokemon_data WHERE against_{type} > 1 AND name = ?", (arg,))
+        collection = table.fetchall()
+
+        if collection:
+            strong_valuesA.append(type)
+
+    weak_values = []
+    for type in types:
+        table.execute(f"SELECT against_{type} FROM imported_pokemon_data WHERE against_{type} < 1 AND pokedex_number = ?", (arg,))
+        collection = table.fetchall()
+
+        if collection:
+            weak_values.append(type)
+
+    weak_valuesA = []
+    for type in types:
+        table.execute(f"SELECT against_{type} FROM imported_pokemon_data WHERE against_{type} < 1 AND name = ?", (arg,))
+        collection = table.fetchall()
+
+        if collection:
+            weak_valuesA.append(type)
+
+    if type1 and type2 and name and strong_values and weak_values:
+        print(f"Analyzing {arg}")
+        print(f"{name[0]} ({type1[0]} {type2[0]}) is strong against {strong_values} but weak against {weak_values}.")
+    elif hi and bye and number and strong_valuesA and weak_valuesA:
+        print(f"Analyzing {number[0]}")
+        print(f"{arg} ({hi[0]} {bye[0]}) is strong against {strong_valuesA} but weak against {weak_valuesA}.")
+    else:
+        print(f"Pokedex number {arg} not found.")
 
 answer = input("Would you like to save this team? (Y)es or (N)o: ")
 if answer.upper() == "Y" or answer.upper() == "YES":
